ce27ea8840f50b3094f61d0a1fef13cd1919274c
Daniel Allinson <danielallinson01@gmail.com>
2020-11-05 14:14:36 +0000

    Initial commit

---
+++ desktop/src/io/github/eng12020team24/project1/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.github.eng12020team24.project1.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.github.eng12020team24.project1.AuberGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AuberGame(), config);
+	}
+}
