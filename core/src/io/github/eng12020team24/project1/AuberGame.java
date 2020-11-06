bf118ce757df069e296c408871203516b2109f59
Mahir Rahman <mahirrahman2000@gmail.com>
2020-11-06 00:31:15 +0000

    Removal of Mahirs git repository within a git repository

--- core/src/io/github/eng12020team24/project1/AuberGame.java
+++ core/src/io/github/eng12020team24/project1/AuberGame.java
@@ -4,24 +4,39 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class AuberGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+
+	private TextureAtlas textureAtlas;
+	public Animation<TextureRegion> auber_walk;
+	public Animation<TextureRegion> star_twinkle;
+	private float elapsedTime = 0f;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
+		textureAtlas = new TextureAtlas(Gdx.files.internal("spritesheet/myspritesheet.atlas"));
+		auber_walk = new Animation<TextureRegion>(1f/4f, textureAtlas.findRegions("AUBER_WALK"));
+		star_twinkle = new Animation<TextureRegion>(1f/8f, textureAtlas.findRegions("SPACE_BG"));
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		elapsedTime += Gdx.graphics.getDeltaTime();;
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(star_twinkle.getKeyFrame(elapsedTime,true),100,100);
+		batch.draw(auber_walk.getKeyFrame(elapsedTime,true), 132,100);
 		batch.end();
 	}
 	
--- desktop/src/io/github/eng12020team24/project1/desktop/DesktopLauncher.java
+++ desktop/src/io/github/eng12020team24/project1/desktop/DesktopLauncher.java
@@ -8,5 +8,6 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new AuberGame(), config);
+		config.foregroundFPS = 60;
 	}
 }
