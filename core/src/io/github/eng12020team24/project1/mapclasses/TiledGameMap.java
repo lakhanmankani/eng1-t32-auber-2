5ee2077de4d11026b5d123b8530b97bb14bc509a
Mahir Rahman <mahirrahman2000@gmail.com>
2020-11-17 00:47:40 +0000

    Added JavaDoc style comments to the classes: GameMaps, TiledGameMaps, TiledType and Minimap. Plus the teleport in Auber.

--- core/src/io/github/eng12020team24/project1/mapclasses/TiledGameMap.java
+++ core/src/io/github/eng12020team24/project1/mapclasses/TiledGameMap.java
@@ -8,15 +8,25 @@ import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class TiledGameMap extends GameMap {
-
+    /**A libgdx class that is used to load and render a tiled map.**/
     TiledMap tiledMap;
+    /**A libgdx class to render orthogonal tile maps- 2D topdown tiled maps.**/
     OrthogonalTiledMapRenderer tiledMapRenderer;
 
+    /**
+     * Initialisation method: uses Superclass's paramaters. See GameMap.
+     * A tmx loader class method load is used to turn a tmx filetype into a tiledmap object.
+     * Creates a camera renderer for the tiledmap provided in tiled libraries
+     */
     public TiledGameMap() {
         tiledMap = new TmxMapLoader().load("maps/Space_Station.tmx");
         tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
     }
 
+    /**
+     * render method to draw the tiledmap using the tilemaprenderer
+     * @param camera pass the game's camera to render according the the viewport of the camera.
+     */
     @Override
     public void render(OrthographicCamera camera) {
         tiledMapRenderer.setView(camera);
@@ -49,6 +59,13 @@ public class TiledGameMap extends GameMap {
         return tiledMap.getLayers().getCount();
     }
 
+    /**
+     * Get the tile from coordinates on the tiledmap, returns the tile's ID.
+     * @param layer Layer of the tile, this can be seen in the tmx.
+     * @param col   Column or x-axis of the tiledmap
+     * @param row   Row or y-axes of the tiledmap
+     * @return  return the tiletype's ID at those coordinates
+     */
     @Override
     public TileType getTileTypeByCoordinate(int layer, int col, int row) {
         TiledMapTileLayer.Cell cell = ((TiledMapTileLayer) tiledMap.getLayers().get(layer)).getCell(col, row);
@@ -63,3 +80,24 @@ public class TiledGameMap extends GameMap {
         return null;
     }
 }
+//MIT License
+//
+//Copyright (c) 2018 Nathanael Maher
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in all
+//copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//SOFTWARE.
