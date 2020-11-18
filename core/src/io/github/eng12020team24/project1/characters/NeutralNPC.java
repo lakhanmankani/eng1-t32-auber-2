ffeed3aa9db211e4c34865d999ba72a2251a5f23
Daniel Allinson <danielallinson01@gmail.com>
2020-11-18 14:07:11 +0000

    Added multiple neutral NPCs

--- core/src/io/github/eng12020team24/project1/characters/NeutralNPC.java
+++ core/src/io/github/eng12020team24/project1/characters/NeutralNPC.java
@@ -1,6 +1,5 @@
 package io.github.eng12020team24.project1.characters;
 
-import io.github.eng12020team24.project1.pathfinding.MapRegion;
 import io.github.eng12020team24.project1.pathfinding.Tile;
 import io.github.eng12020team24.project1.pathfinding.TileGraph;
 
@@ -8,19 +7,20 @@ import java.util.Random;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Vector2;
 
 public class NeutralNPC extends NPCCharacter {
-    private float currentWaitTime;
+    private float currentWaitTime; // The current time to wait for before moving again
     private float elapsedWaitTime;
 
     public NeutralNPC(TileGraph tileGraph, int x, int y, TextureAtlas textureAtlas) {
         super(tileGraph, x, y);
         walkingAnimation = new Animation<TextureRegion>(1f / 4f, textureAtlas.findRegions("Z_NPC_WALK"));
         idleTexture = new TextureRegion(textureAtlas.findRegion("Z_NPC_WALK"));
+    }
+    public NeutralNPC(TileGraph tileGraph, Tile tilePos, TextureAtlas textureAtlas) {
+        this(tileGraph, (int) tilePos.getCenterPosition().x, (int) tilePos.getCenterPosition().y, textureAtlas);
     }
 
     public void move() {
