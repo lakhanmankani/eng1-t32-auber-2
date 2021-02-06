1969fb6c2db7c1213455955c4488e1b8d66ac4da
Lakhan Mankani <lakhan.mankani@gmail.com>
2021-02-06 12:30:46 +0000

    Improve power up tile detection and testing
    Signed-off-by: Lakhan Mankani <lakhan.mankani@gmail.com>

--- tests/src/de/tomgrill/gdxtesting/PowerUpTest.java
+++ tests/src/de/tomgrill/gdxtesting/PowerUpTest.java
@@ -11,8 +11,7 @@ import org.junit.Test;
 //import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 @RunWith(GdxTestRunner.class)
 public class PowerUpTest {
@@ -29,20 +28,60 @@ public class PowerUpTest {
     @Test
     //@DisplayName("Auber should be able to pick up power ups")
     public void testValidPickUp() {
-
-        auber.setX(4*TileType.TILE_SIZE);
-        auber.setY(3*TileType.TILE_SIZE);
-
         PowerUp powerUp = new PowerUp("MultiBeam", 4, 4);
-        assertFalse(powerUp.auberOnPowerUpTile(auber));
+        assertEquals(powerUp.getxPos(), 4*TileType.TILE_SIZE);
+        assertEquals(powerUp.getyPos(), 4*TileType.TILE_SIZE);
 
+        // Test on power up
+        auber.setX(4*TileType.TILE_SIZE);
         auber.setY(4*TileType.TILE_SIZE);
         assertTrue(powerUp.auberOnPowerUpTile(auber));
+
+        // Test off by 1 x
+        auber.setX(2*TileType.TILE_SIZE);
+        auber.setY(4*TileType.TILE_SIZE);
+        assertFalse(powerUp.auberOnPowerUpTile(auber));
+
+        auber.setX(6*TileType.TILE_SIZE);
+        auber.setY(4*TileType.TILE_SIZE);
+        assertFalse(powerUp.auberOnPowerUpTile(auber));
+
+        // Test off by 1 y
+        auber.setX(4*TileType.TILE_SIZE);
+
+        auber.setY(2*TileType.TILE_SIZE);
+        assertFalse(powerUp.auberOnPowerUpTile(auber));
+
+        auber.setY(6*TileType.TILE_SIZE);
+        assertFalse(powerUp.auberOnPowerUpTile(auber));
     }
 
     @Test
     //@DisplayName("Auber should be able to use power ups")
     public void testUsePowerUp() {
+    }
+
+    @Test
+    public void voidTestPowerUpTimer() {
+        PowerUp powerUp = new PowerUp("MultiBeam", 4, 4, 30);
+
+        // Test timer constructor
+        assertFalse(powerUp.finishedUsing());
+        assertEquals(powerUp.getTimer(), 30.0, 1);
+
+        // Test set timer
+        powerUp.setTimer(20);
+        assertFalse(powerUp.finishedUsing());
+        assertEquals(powerUp.getTimer(), 20.0, 1);
+
+        // Test start using
+        powerUp.startUsing();
+        assertFalse(powerUp.finishedUsing());
+        assertEquals(powerUp.getTimer(), 30.0, 1);
+
+        // Test finished using
+        powerUp.setTimer(0);
+        assertTrue(powerUp.finishedUsing());
     }
 
 }
