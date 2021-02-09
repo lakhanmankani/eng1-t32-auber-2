7485fcd0492b492b82ebc4c84ee857761103e36c
gigabytegoob <44292922+gigabytegoob@users.noreply.github.com>
2021-02-09 00:11:49 +0000

    Create ArrestingTest.java

---
+++ tests/src/de/tomgrill/gdxtesting/ArrestingTest.java
@@ -0,0 +1,12 @@
+package de.tomgrill.gdxtesting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(GdxTestRunner.class)
+public class ArrestingTest {
+
+    @Test
+    public void testArresting() {
+    }
+}
