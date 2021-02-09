b592f3733e179fa7202d74c33112acb080cf8262
gigabytegoob <44292922+gigabytegoob@users.noreply.github.com>
2021-02-09 00:11:56 +0000

    Create GameEndTest.java

---
+++ tests/src/de/tomgrill/gdxtesting/GameEndTest.java
@@ -0,0 +1,26 @@
+package de.tomgrill.gdxtesting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(GdxTestRunner.class)
+public class GameEndTest {
+
+//    AuberGame game;
+//    MenuState menu;
+//    LoadSystem load;
+
+    @Test
+    public void testCriticalSystemsDestroyedEnd() {
+//        ActualGame actualGame = new ActualGame(game, 0, menu, load, false);
+//        actualGame.render(0);
+//        game.getScreen();
+
+        // Test for GameOverState
+    }
+
+    @Test
+    public void testAllInfilitratorsArrestedEnd() {
+        // Test that 8 infiltrators arrested
+    }
+}
