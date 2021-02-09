c5d6c15a8d199cbd5baee1e8c03d83ba6e707d84
gigabytegoob <44292922+gigabytegoob@users.noreply.github.com>
2021-02-09 11:44:09 +0000

    Update GameEndTest.java

--- tests/src/de/tomgrill/gdxtesting/GameEndTest.java
+++ tests/src/de/tomgrill/gdxtesting/GameEndTest.java
@@ -1,24 +1,35 @@
 package de.tomgrill.gdxtesting;
 
+import io.github.eng12020team24.project1.characters.Infiltrator;
 import io.github.eng12020team24.project1.gamestates.ActualGame;
 import io.github.eng12020team24.project1.gamestates.AuberGame;
+import io.github.eng12020team24.project1.system.StationSystem;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(GdxTestRunner.class)
 public class GameEndTest {
 
     @Test
-    public void testCriticalSystemsDestroyedEnd() {
+    public void testAllInfilitratorsArrestedEnd() {
         AuberGame testGame = new AuberGame();
         ActualGame actualGame = new ActualGame(testGame, 0, testGame.menuState, null, false, true);
-//        int numOfInfiltrators = actualGame.infiltratorsToAdd.size();
-//        assertEquals(numOfInfiltrators, 8);
-
+        actualGame.infiltratorsToAdd = new ArrayList<Infiltrator>();
+        actualGame.infiltrators = new ArrayList<Infiltrator>();
+        actualGame.render(0);
+        assertEquals(actualGame.end, "Win");
     }
 
     @Test
-    public void testAllInfilitratorsArrestedEnd() {
-        // Test that 8 infiltrators arrested
+    public void testCriticalSystemsDestroyedEnd() {
+        AuberGame testGame = new AuberGame();
+        ActualGame actualGame = new ActualGame(testGame, 0, testGame.menuState, null, false, true);
+        actualGame.stationSystems = new ArrayList<StationSystem>();
+        actualGame.render(0);
+        assertEquals(actualGame.end, "Lose");
     }
 }
